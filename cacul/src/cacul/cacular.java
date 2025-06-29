34ce79cfcabe5d5baa5c3b712485f7e657ead675
AnNguyen264IT <npan264work@gmail.com>
2025-06-29 22:41:49 +0700

    multiply

--- cacul/src/cacul/cacular.java
+++ cacul/src/cacul/cacular.java
@@ -1,8 +1,8 @@
 package cacul;
 
 public class cacular {
-	public static int add(int a, int b) {
-        return a + b;
+	public static int multiply(int a, int b) {
+        return a * b;
     }
 
   
--- cacul/src/cacul/main.java
+++ cacul/src/cacul/main.java
@@ -5,8 +5,7 @@ public class main {
      int a = 12;
      int b = 4;
 
-     System.out.println("Trừ: " + cacular.add(a, b));
-//     System.out.println("Nhân: " + Calculator.multiply(a, b));
+     System.out.println("Nhân: " + cacular.multiply(a,b));
 //     System.out.println("Chia: " + Calculator.divide(a, b));
 }
 }
