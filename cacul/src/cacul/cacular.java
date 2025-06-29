5f6098112294bc12a0b7a5908606dcab8095f6f4
AnNguyen264IT <npan264work@gmail.com>
2025-06-29 22:37:03 +0700

    substract

---
+++ cacul/src/cacul/cacular.java
@@ -0,0 +1,10 @@
+package cacul;
+
+public class cacular {
+	public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+  
+}
+
---
+++ cacul/src/cacul/main.java
@@ -0,0 +1,12 @@
+package cacul;
+
+public class main {
+ public static void main(String[] args) {
+     int a = 12;
+     int b = 4;
+
+     System.out.println("Trừ: " + cacular.subtract(a, b));
+//     System.out.println("Nhân: " + Calculator.multiply(a, b));
+//     System.out.println("Chia: " + Calculator.divide(a, b));
+}
+}
